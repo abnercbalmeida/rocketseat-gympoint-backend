640f228072afdc025071241e1bea76ed1bff5aaf
Abner <abnercbalmeida@gmail.com>
2019-11-18 21:34:19 -0300

    add plans CRUD; add plans migration

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,66 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {
+    const plans = await Plan.findAll();
+
+    return res.json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number()
+        .integer()
+        .required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const plan = await Plan.create(req.body);
+
+    return res.json(plan);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number().integer(),
+      price: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+    const plan = await Plan.findByPk(id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan not found' });
+    }
+
+    await plan.update(req.body);
+
+    return res.json(plan);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const plan = await Plan.findByPk(id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan not found' });
+    }
+
+    await Plan.destroy({ where: { id } });
+
+    return res.status(204).json();
+  }
+}
+
+export default new PlanController();
